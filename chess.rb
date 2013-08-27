17ef19635944f1e15eabc3d193b2b6a0623ac976
App Academy Student <student@appacademy.io>
2013-08-27 14:16:34 -0400

    Adds user input, conversion, and validation for from_coord is empty and same color on to_square. Put board methods in Board class.

--- chess.rb
+++ chess.rb
@@ -1,8 +1,41 @@
 class ChessGame
-  attr_accessor :board
+  attr_accessor :board, :turn, :players
   # variables:  turn, board, players
   def initialize
+    @board = Board.new
+    @turn = :white
+    @players = {white: HumanPlayer.new(:white), black: HumanPlayer.new(:black)}
+  end
+
+  def play
+    # get moves and make moves until game is won
+    until board.won? || board.draw?
+      board.render
+
+      players[turn].get_move
+      make_move
+    end
+  end
+end
+
+class Board
+  attr_accessor :board
+
+  def initialize
     @board = new_board
+  end
+
+  def [](*args)
+    if args.length == 2
+      board[args[0]][args[1]]
+    elsif args.length == 1
+      board[args[0][0]][args[0][1]]
+    else
+      raise StandardError "Square brackets needs 1 or 2 arguments."
+    end
+  rescue StandardError => e
+    puts e.message
+    puts args
   end
 
   def new_board
@@ -45,7 +78,7 @@ class ChessGame
     end
   end
 
-  def display_board
+  def render
     board.each do |row|
       row.each do |piece|
         if piece
@@ -58,7 +91,43 @@ class ChessGame
     end
   end
 
-  def play
+  def valid?(move) # ["f2", "f3"]
+    from_coord, to_coord = convert(move)
+
+    return false unless self[from_coord]
+    #check for piece of same color on end square
+    from_piece = self[from_coord]
+
+    if self[to_coord] #theres a piece at to spot
+      to_piece = self[to_coord]
+      return false if to_piece.color == from_piece.color
+      #the piece is attacking
+    end
+
+    #make sure to_square is included in valid_moves of piece at from_square
+
+    true
+
+    #hypothetically make move to see if king was in check
+  end
+
+  def convert(move) # ["f2", "f3"] => [[6,5],[5,5]]
+    move.map! do |coord|
+      col = ("a".."h").to_a.index(coord[0])
+      [8-coord[1].to_i, col]
+    end
+
+    move
+  end
+
+  def won?
+    # check that king is under attack and there is no move to uncheck
+    false
+  end
+
+  def draw?
+    # there's no legal move
+    false
   end
 end
 
@@ -143,12 +212,39 @@ class King < Piece
   end
 end
 
-
 class HumanPlayer
-  # vars: color
+  attr_accessor :color
+
+  def initialize(color)
+    @color = color
+  end
+
+  def get_move
+    puts "Enter your move. Ex: 'f2 f3'"
+    answer = gets.chomp.split
+
+    unless board.valid?(answer)
+      StandardError.new "Invalid move. Try again loser!"
+    end
+  rescue StandError => e
+    puts e.message
+    retry
+  end
+
 end
 
 if __FILE__ == $0
-  c = ChessGame.new
-  c.display_board
+  b = ChessGame.new.board
+end
+
+module StraightMover
+  #how rooks, kings, queens move
+end
+
+module DiagMover
+  #how bishops, kings, and queens move
+end
+
+module MultiMover
+  #how bishops, rooks, and queens move
 end
